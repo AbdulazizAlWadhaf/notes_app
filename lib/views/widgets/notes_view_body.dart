59c594e27bf738d54f9c6ece2f5913ce7568a4b4
AbdulazizAlWadhaf <abdulazizalwadhaf123@gmail.com>
2025-05-10 01:17:26 -0700

    custom note item

---
+++ lib/views/widgets/notes_view_body.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_app_bar.dart';
+import 'custom_note_item.dart';
+
+
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: const Column(
+        children: const [
+          SizedBox(height: 50,),
+          CustomAppBar(),
+          NoteItem()
+        ],
+      ),
+    );
+  }
+}
+
+
