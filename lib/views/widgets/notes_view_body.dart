7ba0475be80b85525c1dffb1196012833e968d44
AbdulazizAlWadhaf <abdulazizalwadhaf123@gmail.com>
2025-05-10 02:25:30 -0700

    Notes ListView

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
-import 'custom_note_item.dart';
+import 'notes_list_view.dart';
 
 
 
@@ -15,11 +15,12 @@ class NotesViewBody extends StatelessWidget {
         children: const [
           SizedBox(height: 50,),
           CustomAppBar(),
-          NoteItem()
+          Expanded(child: NotesListView())
         ],
       ),
     );
   }
 }
+
 
 
